4b700065d98be826dcf461677e4ebfc4c0796b73
harry_jumeau <hjumeau.prestataire@vsc-technologies.com>
2013-09-23 14:15:13 +0200

    Bootstrap according to the environment

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -6,23 +6,26 @@ import com.howmanypeople.authentication.User
 class BootStrap {
 
     def init = { servletContext ->
+        environments {
+            production {
+                def adminRole = new Role(authority: 'ROLE_ADMIN', description: 'Utilisateur').save()
+                def userRole = new Role(authority: 'ROLE_USER', description: 'Utilisateur').save()
 
-//		def adminRole = new Role(authority: 'ROLE_ADMIN', description: 'Utilisateur').save()
-//		def userRole = new Role(authority: 'ROLE_USER', description: 'Utilisateur').save()
-//		
-//		def admin = new User(username: 'admin',
-//			password: 'password',
-//			email:'admin@howmanypeople.com',
-//			enabled: true)
-//		admin.save()
-//		Authorization.create(admin, adminRole, true)
-//		
-//		def user = new User(username: 'harry',
-//			password: 'password',
-//			email:'harry@howmanypeople.com',
-//			enabled: true)
-//		user.save()
-//		Authorization.create(user, userRole, true)
+                def admin = new User(username: 'admin',
+                        password: 'password',
+                        email:'admin@howmanypeople.com',
+                        enabled: true)
+                admin.save()
+                Authorization.create(admin, adminRole, true)
+
+                def user = new User(username: 'harry',
+                        password: 'password',
+                        email:'harry@howmanypeople.com',
+                        enabled: true)
+                user.save()
+                Authorization.create(user, userRole, true)
+            }
+        }
     }
     def destroy = {
     }
