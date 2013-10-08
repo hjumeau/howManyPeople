3d226227af810f0e80f9e70baf0f6b1b04302053
harry_jumeau <hjumeau.prestataire@vsc-technologies.com>
2013-10-08 09:52:51 +0200

    Add fonts for icon

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -10,7 +10,6 @@ modules = {
 		defaultBundle 'app'
         resource url:'js/Application.js'
         resource url:'js/authentication/User.js'
-        resource url:'js/ApplicationController.js'
         resource url:'js/authentication/AuthenticatedRoute.js'
 		resource url:'js/authentication/LoginController.js'
 		resource url:'js/Router.js'
@@ -26,7 +25,10 @@ modules = {
         resource url:'js/ApplicationView.js'
     }
 	style {
+		resource url:'fonts/icons/style.css'
+		resource url:'css/bootstrap.css'
 		resource url:'css/structure.less', attrs:[rel: "stylesheet/less", type:'css'], bundle:'bundle_style'
+		resource url:'css/login.less', attrs:[rel: "stylesheet/less", type:'css'], bundle:'bundle_style'
 		resource url:'css/header.less', attrs:[rel: "stylesheet/less", type:'css'], bundle:'bundle_style'
 		resource url:'css/needforbundle.css'
 	}
