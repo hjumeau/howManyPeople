76ba8f974029f8a5a642ad671daad7ff6e667d3e
harry_jumeau <hjumeau.prestataire@vsc-technologies.com>
2013-10-23 18:48:09 +0200

    Start refacto app.user in ember js

--- web-app/js/Application.js
+++ web-app/js/Application.js
@@ -1,3 +1,16 @@
 App = Ember.Application.create({
 	LOG_TRANSITIONS: true
 });
+
+Ember.Application.initializer({
+    name: "ioc",
+
+    initialize: function(container, application) {
+        container.register('session:session', App.User);
+        container.optionsForType('session', { singleton: true });
+
+        // inject session in all controllers and routes
+        container.typeInjection('controller', 'session', 'session:session');
+        container.typeInjection('route', 'session', 'session:session');
+    }
+});
